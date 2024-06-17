68b5554e8e0794f04057172f24f7d5c5ba86ed09
long <longle35610@gmail.com>
2024-06-17 16:49:59 +0700

    update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,7 +8,6 @@ export async function GET(req: NextRequest) {
     const response = await fetch(`${BASE_URL}/api/user/profile`, {
         headers: {
             'Content-Type': 'application/json',
-            "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3VzZXIvbG9naW4iLCJpYXQiOjE3MTYyODIyODIsImV4cCI6MTcxNjI4NTg4MiwibmJmIjoxNzE2MjgyMjgyLCJqdGkiOiJzSW5oanA0Z2p1U2tTVTc4Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.T4frq-ZrkYk6LA0JSWrRiLixpUF61wVunobMl7_kpW4`
         },
     });
     const data = await response.json();
